2474921694d17baeb63b49ddea3a67eb789805ad
Maxime Tenev <maxime111208@gmail.com>
2024-08-05 17:21:43 +0200

    Update Guess_the_number.py
    Added a loop, so the user can play again

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,24 +1,31 @@
 import random
 import shelve
-secret_number=random.randint(1,100)
-user_try=0
-nb_attempt=0
-print("What number am I thinking of ?")
-while user_try!=secret_number:
-    user_try=int(input("Try :"))
-    if user_try < secret_number:
-        print("Too low !")
-        nb_attempt+=1
-    elif user_try > secret_number:
-        print("Too high !")
-        nb_attempt+=1
-    else:
-        print("Correct !")
-        nb_attempt+=1
-        print(f'You did it in {nb_attempt} tries !')
-with shelve.open('highscore') as highscore:
-        best_score = highscore['best_score']
-        print(f'The highscore is in {best_score} tries')
-        if best_score is None or nb_attempt < best_score:
-            highscore['best_score'] = nb_attempt    
-            print(f'New highscore: {nb_attempt} tries!')
+while True:
+        secret_number=random.randint(1,100)
+        user_try=0
+        nb_attempt=0
+        print("What number between 1 to 100 am I thinking of ?")
+        while user_try!=secret_number:
+            user_try=int(input("Try :"))
+            if user_try < secret_number:
+                print("Too low !")
+                nb_attempt+=1
+            elif user_try > secret_number:
+                print("Too high !")
+                nb_attempt+=1
+            else:
+                print("Correct !")
+                nb_attempt+=1
+                print(f'You did it in {nb_attempt} tries !')
+
+        with shelve.open('highscore') as highscore:
+                try:
+                    best_score = highscore['best_score']
+                except KeyError:
+                    best_score = None
+                print(f'The highscore is in {best_score} tries')
+                if best_score is None or nb_attempt < best_score:
+                    highscore['best_score'] = nb_attempt    
+                    print(f'New highscore: {nb_attempt} tries!')
+
+        input("Press enter to restart ...")
